7b6b49b2fdd35f619d41d83ddd0c17f47d5d08c0
Faria Waseer <fariawaseer@gmail>
2025-09-24 11:39:08 +0500

    day 2

---
+++ challenges/day2.js
@@ -0,0 +1,43 @@
+// Activity 1
+
+const num1 = 4 
+const num2 = 6
+console.log(`4 + 6 = ${num1 + num2}`) // 10
+console.log(`4 - 6 = ${num1 - num2}`) // -2
+console.log(`4 * 6 = ${num1 * num2}`) // 24
+console.log(`4 / 6 = ${num1 / num2}`) // 0.66
+console.log(`4 % 6 = ${num1 % num2}`) // 4 -- in js remainder if a is lessthan b than (a) is the result 
+
+// Activity 2
+let myNum = 4
+let myNumResult =1
+
+console.log(`+= operator ${myNum += myNumResult }`) // 5
+console.log(`-= operator ${myNum -= myNumResult }`) // 4 --- because here myNum is 5 so (5 - 1) = 4 
+
+// Activity 3
+
+const valOne = 4
+const valTwo = 6
+console.log(`${valOne} > ${valTwo} ${valOne > valTwo}`) // 4 > 6 false
+console.log(`${valOne} < ${valTwo} ${valOne < valTwo}`) // 4 < 6 true
+console.log(`${valOne} >= ${valTwo} ${valOne >= valTwo}`) // 4 >= 6 false
+console.log(`${valOne} <= ${valTwo} ${valOne <= valTwo}`) // 4 <= 6 true
+console.log(`${valOne} == ${valTwo} ${valOne == valTwo}`) //4 == 6 false
+console.log(`${valOne} === ${valTwo} ${valOne === valTwo}`) //4 === 6 false
+
+// Activity 4
+const logicOne = true
+const logicTwo = false
+
+console.log(`${logicOne && logicTwo}`) // false
+console.log(`${logicOne || logicTwo}`) // true
+console.log(`${!logicOne}`) // false
+
+
+// Activity 5 
+
+const value = 4
+const myvalue = 2
+console.log(value > myvalue ? "true" : "false")
+

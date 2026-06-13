aaf4915d13ae497ec44418a41c08f6118f127d78
Faria Waseer <fariawaseer@gmail.com>
2026-06-13 23:22:20 +0500

    JS Questions

---
+++ 21DaysJSChallenge/Day1.js
@@ -0,0 +1,55 @@
+const prompt = require("prompt-sync")()
+// // Print Numbers from 1 to N
+// let num = Number(prompt("Enter a number "))
+// for (let i = 1; i <= num; i++) {
+//     console.log(i) 
+// } 
+
+// Print Numbers from N to 1 without changing the loop condition of above question
+// let lessNum = Number(prompt("Enter You'r number "))
+// for(let i = 1; i <= lessNum; i++){
+//     console.log(lessNum - i + 1)
+// }
+
+// Print All Even Numbers from 1 to N
+// const evenNum = Number(prompt("Enter Number "))
+
+// for(let i = 0; i<= evenNum; i++){
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// Sum of First n Natural Numbers
+// solution 1
+// console.time()
+// const NaturalNumA = 1000000000n
+// let sumOfNaturalNum = 0;
+// for(let i = 1; i <= NaturalNumA; i++){
+//     sumOfNaturalNum += i;
+// }
+//  console.log(sumOfNaturalNum)
+// console.timeEnd()
+
+// solution 2
+// console.time()
+// const NaturalNumb = 1000000000
+// let sumOfNaturalNum = ( NaturalNumb * (NaturalNumb + 1)) / 2
+//  console.log(sumOfNaturalNum)
+// console.timeEnd()
+
+// Product (Factorial) of N
+// const factorialNum = Number(prompt("Enter number here: "))
+// let factorial = 1;
+// for (let i = 1; i <= factorialNum; i++) {
+//     console.log(factorial *= i)
+// }
+
+// Sum of All Even Numbers up to N
+// const evenNum = Number(prompt("Enter num: "))
+// let evenSum = 0
+// for (let i = 0; i <= evenNum; i++) {
+//     if(i % 2 === 0) evenNum += i
+// }
+//  console.log(evenSum)
+

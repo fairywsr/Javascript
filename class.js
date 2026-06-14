9e8be859c3f591a390460b6407ee73b8dac2d14b
Faria Waseer <fariawaseer@gmail.com>
2026-06-14 23:55:58 +0500

    strings

--- class.js
+++ class.js
@@ -1,6 +1,10 @@
-let num = 5;
-let fact = 1;
-for (let i = 1; i <= num; i++) {
-  fact *= i;
-}
-console.log(fact);
+// let num = 5;
+// let fact = 1;
+// for (let i = 1; i <= num; i++) {
+//   fact *= i;
+// }
+// console.log(fact);
+
+// 14/6/2026
+// vowels count in string
+const word = "javascript";

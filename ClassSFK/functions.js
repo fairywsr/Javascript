a7cec7e0999686e4dc4b00314793d54632559b05
Faria Waseer <fariawaseer@gmail.com>
2026-06-15 23:56:35 +0500

    functions

---
+++ ClassSFK/functions.js
@@ -0,0 +1,18 @@
+// function printName(name) {
+//   console.log(name);
+// }
+
+// printName("faria");
+
+// const table = (table, limit) => {
+//   for (let i = 1; i <= limit - 1 - 1; i++) {
+//     console.log(`${table} X ${i} = ${table * i}`);
+//   }
+// };
+// table(2, 6);
+
+const capitalFirstChar = (string) => {
+  return string.charAt(0).toUpperCase();
+};
+
+capitalFirstChar("Faria");

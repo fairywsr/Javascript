5c8e0428d7d3ba413aaf76ff7f12bf89a54e7ae9
Faria Waseer <fariawaseer@gmail.com>
2026-06-22 12:36:26 +0500

    arrays of objects

---
+++ ClassSFK/array.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    title: "t-shirt-A",
+    price: 5000,
+    color: "white",
+    stock: 67,
+    ratings: "*****",
+  },
+  {
+    title: "t-shirt-B",
+    price: 4500,
+    color: "half-white",
+    stock: 60,
+    ratings: "*****",
+  },
+  {
+    title: "t-shirt-C",
+    price: 4000,
+    color: "blue",
+    stock: 57,
+    ratings: "*****",
+  },
+  {
+    title: "t-shirt-D",
+    price: 3500,
+    color: "yellow",
+    stock: 50,
+    ratings: "*****",
+  },
+  {
+    title: "t-shirt-E",
+    price: 3000,
+    color: "green",
+    stock: 47,
+    ratings: "*****",
+  },
+];
+
+// for of
+// for (product of products) {
+//   console.log(product.title, product.price, product.color);
+// }
+
+// map
+// const mapProd = products.map((product) => {
+//   return (product.price, product.color, product.stock);
+// });
+
+// console.log(mapProd);
+
+// filter
+// const filterProd = products.filter((product) => {
+//   return product.price >= 4000;
+// });
+// console.log(filterProd);
+
+// filterProd.map((prod) => {
+//   console.log(prod.color);
+// });
+
+// map + filter + reduce
+const total = products
+  .filter((prod) => {
+    return prod.price >= 4000;
+  })
+  .map((prod) => {
+    return prod;
+  })
+  .reduce((acc, val) => acc + val.price, 0);
+console.log(total);
